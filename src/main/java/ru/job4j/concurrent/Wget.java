8b8d7798e136c0271a6012ca2f1d0172e6ad68ec
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-07 14:51:17 +0900

    1.2. Режим ожидания. [#231217 #225360]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,13 +5,18 @@ import java.time.Year;
 public class Wget {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("\rLoading : " + i + "%");
-        }
+        Thread first = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + i + "%");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        first.start();
     }
 }

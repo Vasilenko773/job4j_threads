08d10a8f7607130baa758edbad3d4d6ab2a8d678
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-08 20:51:46 +0900

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/sinhronization/DCLSingleton.java
@@ -0,0 +1,17 @@
+package ru.job4j.sinhronization;
+
+public class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}

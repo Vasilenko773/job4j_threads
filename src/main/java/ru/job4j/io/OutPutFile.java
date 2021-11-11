3079714dead609d80563e87ade5ab122364c6c6a
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-11 20:39:29 +0900

    1. Visibility. Общий ресурс вне критической секции [#1102 #226184]

--- src/main/java/ru/job4j/io/OutPutFile.java
+++ src/main/java/ru/job4j/io/OutPutFile.java
@@ -1,21 +1,22 @@
 package ru.job4j.io;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class OutPutFile {
 
-    private final ParseFile parseFile;
+    private final File file;
 
 
-    public OutPutFile(ParseFile parseFile) {
-        this.parseFile = parseFile;
+    public OutPutFile(File file) {
+        this.file = file;
     }
 
     public void saveContent(String content) {
         try (BufferedOutputStream out = new BufferedOutputStream(
-                new FileOutputStream(parseFile.getContent()))) {
+                new FileOutputStream(file))) {
             for (int i = 0; i < content.length(); i += 1) {
                 out.write(content.charAt(i));
             }

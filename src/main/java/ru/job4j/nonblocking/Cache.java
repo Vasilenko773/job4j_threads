7661f361595c7ff3559822efe9bd96f0122473bf
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-16 19:17:08 +0900

    1. Неблокирующий кеш [#4741 #228802]

--- src/main/java/ru/job4j/nonblocking/Cache.java
+++ src/main/java/ru/job4j/nonblocking/Cache.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Cache {
+
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
 
     public boolean add(Base model) {
@@ -12,19 +13,18 @@ public class Cache {
 
     public boolean update(Base model) throws OptimisticException {
         Base exp = memory.get(model.getId());
-        if (exp.getVersion() == model.getVersion()) {
-            model.setName("Super-puper");
-            return memory.replace(model.getId(), new Base(model.getId(), model.getVersion() + 1)) != null;
-        } else {
+        if (exp.getVersion() != model.getVersion()) {
             throw new OptimisticException("У сравниваемых моделей не равны версии");
         }
+        model.setName("Super-puper");
+        return memory.replace(model.getId(), new Base(model.getId(), model.getVersion() + 1)) != null;
     }
 
     public void delete(Base model) {
         memory.remove(model.getId(), model);
     }
 
-    static class OptimisticException extends Throwable {
+    static class OptimisticException extends RuntimeException {
 
         public OptimisticException(String string) {
             super(string);

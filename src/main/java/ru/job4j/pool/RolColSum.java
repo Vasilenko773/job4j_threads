d5f0f16fde317edb4d420bd0f04f5910821fbc16
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-20 12:17:50 +0900

    4. CompletableFuture [#361626 #230695]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -30,60 +30,33 @@ public class RolColSum {
     }
 
     public static Sums[] sum(int[][] matrix) {
-        int sumI = 0;
-        Sums[] sum = column(matrix);
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                sumI += matrix[i][j];
-            }
-            Sums sums = sum[i];
-            sums.setRowSum(sumI);
-            sum[i] = sums;
-            sumI = 0;
-        }
-        return sum;
+        return sumRowAndColumn(matrix);
     }
 
-    private static Sums[] column(int[][] matrix) {
-        Sums[] rsl = new Sums[matrix.length];
-        for (int j = 0; j < matrix[0].length; j++) {
+    private static Sums[] sumRowAndColumn(int[][] matrix) {
+        Sums[] rsl = new Sums[matrix.length * 2];
+        int size = 0;
+        for (int i = 0; i < matrix.length; i++) {
+
             int c = 0;
             int sumJ = 0;
             while (c < matrix.length) {
-                sumJ += matrix[c++][j];
+                sumJ += matrix[c++][i];
+            }
+            int sumI = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                sumI += matrix[i][j];
             }
             Sums sums = new Sums();
+            sums.setRowSum(sumI);
             sums.setColSum(sumJ);
-            rsl[j] = sums;
+            rsl[size++] = sums;
         }
         return rsl;
     }
 
-
-    public static Sums[] asyncSum(int[][] matrix)  {
-        CompletableFuture<Sums[]> rsl = CompletableFuture.completedFuture(sum(matrix));
+    public static Sums[] asyncSum(int[][] matrix) {
+        CompletableFuture<Sums[]> rsl = CompletableFuture.completedFuture(sumRowAndColumn(matrix));
         return rsl.join();
-    }
-
-    public static void main(String[] args) {
-        Sums[] array = sum(new int[][]
-                {
-                        {1, 2},
-                        {2, 4}
-                }
-        );
-
-        Sums[] array1 = asyncSum(new int[][]
-                        {
-                                {2, 2, 3},
-                                {1, 4, 7},
-                                {6, 2, 5}
-                        }
-                );
-
-        for (int i = 0; i < array1.length; i++) {
-            System.out.println(array1[i].getRowSum() + " Строка" + i);
-            System.out.println(array1[i].getColSum() + " столбец" + i);
-        }
     }
 }

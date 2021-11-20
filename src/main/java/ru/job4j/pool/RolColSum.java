17d7f9d7ae252b1aa191940b0d0e803324d2a7ee
Нина <vasilenko.lesnik2017@yandex.ru>
2021-11-20 17:03:05 +0900

    4. CompletableFuture [#361626 #230695]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -30,33 +30,38 @@ public class RolColSum {
     }
 
     public static Sums[] sum(int[][] matrix) {
-        return sumRowAndColumn(matrix);
-    }
-
-    private static Sums[] sumRowAndColumn(int[][] matrix) {
         Sums[] rsl = new Sums[matrix.length];
-        int size = 0;
         for (int i = 0; i < matrix.length; i++) {
-
-            int c = 0;
-            int sumJ = 0;
-            while (c < matrix.length) {
-                sumJ += matrix[c++][i];
-            }
-            int sumI = 0;
-            for (int j = 0; j < matrix[i].length; j++) {
-                sumI += matrix[i][j];
-            }
-            Sums sums = new Sums();
-            sums.setRowSum(sumI);
-            sums.setColSum(sumJ);
-            rsl[size++] = sums;
+            rsl[i] = sumRowAndColumn(matrix, i);
         }
         return rsl;
     }
 
+    private static Sums sumRowAndColumn(int[][] matrix, int index) {
+        int c = 0;
+        int sumJ = 0;
+        int sumI = 0;
+        while (c < matrix.length) {
+            sumJ += matrix[c++][index];
+            c--;
+            sumI += matrix[index][c++];
+        }
+        Sums sums = new Sums();
+        sums.setRowSum(sumI);
+        sums.setColSum(sumJ);
+        return sums;
+    }
+
     public static Sums[] asyncSum(int[][] matrix) {
-        CompletableFuture<Sums[]> rsl = CompletableFuture.completedFuture(sumRowAndColumn(matrix));
+        CompletableFuture<Sums[]> rsl = CompletableFuture.supplyAsync(
+                () -> {
+                    Sums[] exp = new Sums[matrix.length];
+                    for (int i = 0; i < matrix.length; i++) {
+                        exp[i] = sumRowAndColumn(matrix, i);
+                    }
+                    return exp;
+                }
+        );
         return rsl.join();
     }
 }
